9064efda76953470027b11b434abe269ec830872
Hustler <waiminlsolso@gmail.com>
2019-08-09 22:09:52 +0630

    REST-Based React Apps

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React, { Component } from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { Router, Route } from "react-router-dom";
 import StreamCreate from "./Components/Streams/StreamCreate";
 import StreamDelete from "./Components/Streams/StreamDelete";
 import StreamList from "./Components/Streams/StreamList";
 import StreamShow from "./Components/Streams/StreamShow";
 import StreamEdit from "./Components/Streams/StreamEdit";
 import Header from "./Components/Header";
+import history from './history';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <BrowserRouter>
+        <Router history={history
+        }>
           <Header />
           <Route path="/" exact component={StreamList} />
           <Route path="/streams/new" component={StreamCreate} />
-          <Route path="/streams/edit" component={StreamEdit} />
+          <Route path="/streams/edit/:id" component={StreamEdit} />
           <Route path="/streams/delete" component={StreamDelete} />
           <Route path="/streams/show" component={StreamShow} />
-        </BrowserRouter>
+        </Router>
       </div>
     );
   }
