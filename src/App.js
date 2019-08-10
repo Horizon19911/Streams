ff4c0a93d7af68c1840478130bd3aa261424080c
Hustler <waiminlsolso@gmail.com>
2019-08-10 13:22:06 +0630

    Live Stream With RTMP Server

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React, { Component } from "react";
-import { Router, Route } from "react-router-dom";
+import { Router, Route, Switch } from "react-router-dom";
 import StreamCreate from "./Components/Streams/StreamCreate";
 import StreamDelete from "./Components/Streams/StreamDelete";
 import StreamList from "./Components/Streams/StreamList";
 import StreamShow from "./Components/Streams/StreamShow";
 import StreamEdit from "./Components/Streams/StreamEdit";
 import Header from "./Components/Header";
-import history from './history';
+import history from "./history";
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <Router history={history
-        }>
+        <Router history={history}>
           <Header />
-          <Route path="/" exact component={StreamList} />
-          <Route path="/streams/new" component={StreamCreate} />
-          <Route path="/streams/edit/:id" component={StreamEdit} />
-          <Route path="/streams/delete/:id" component={StreamDelete} />
-          <Route path="/streams/show" component={StreamShow} />
+          <Switch>
+            <Route path="/" exact component={StreamList} />
+            <Route path="/streams/new" exact component={StreamCreate} />
+            <Route path="/streams/edit/:id" exact component={StreamEdit} />
+            <Route path="/streams/delete/:id" exact component={StreamDelete} />
+            <Route path="/streams/:id" exact component={StreamShow} />
+          </Switch>
         </Router>
       </div>
     );
